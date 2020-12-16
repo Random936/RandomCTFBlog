891728ad042a48d4a972e03f95851737d909e972
Random936 <65585087+Random936@users.noreply.github.com>
2020-12-16 14:36:11 -0800

    Added a database and some content to the admin page
    Incorporated NeDB as the database for users. Also added a signup process for members. The admin page now has a table as well to see all users.

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
 const app = express()
+const Datastore = require('nedb')
+const db = new Datastore({ filename: 'database.db', autoload: true })
 
 // Change before production!
 const jwtsecret = "q3HKVf5TG2ez4KSeBlPXWRWQca3B5FNrPF0BHGPF"
-
-const users = [{
-    username: "administrator",
-    password: "password123",
-    isadmin: true
-},{
-    username: "member",
-    password: "secret123",
-    isadmin: false
-}]
 
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -27,29 +19,38 @@ app.get('/', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
+    
     console.log("Username: " + req.body.username + "\tPassword: " + req.body.password)
-    const user = users.find((user) => {
-        return user.username === req.body.username && user.password === req.body.password
-    })
 
-    if (user) {
-        const jwtToken = jwt.sign(
-            {username: user.username, isadmin: user.isadmin},
-            jwtsecret
-        )
+    db.findOne({ 
+        username: req.body.username, 
+        password: req.body.password
+    },(err, user) => {
+        if (err) {return undefined}
 
-        res.cookie("login_token", jwtToken, {maxAge: 2592000000})
-        if (user.isadmin === true) {
-            console.log("Admin login successful.")
-            res.redirect('/admin')
+        if (user) {
+
+            const jwtToken = jwt.sign(
+                {username: user.username, isadmin: user.isadmin},
+                jwtsecret
+            )
+    
+            res.cookie("login_token", jwtToken, {maxAge: 2592000000})
+
+            if (user.isadmin === true) {
+                console.log("Admin login successful.")
+                res.redirect('/admin')
+            } else {
+                console.log("Member login successful.")
+                res.redirect('/member')
+            }
+    
         } else {
-            console.log("Member login successful.")
-            res.redirect('/member')
+            res.redirect('/')
         }
 
-    } else {
-        res.redirect('/')
-    }
+    })
+
 })
 
 app.get('/logout', (req, res) => {
@@ -57,39 +58,90 @@ app.get('/logout', (req, res) => {
     res.redirect('/')
 })
 
+app.get('/signup', (req, res) => {
+    res.render('signup.ejs', { loginmessage: "" })
+})
+
+app.post('/signup', (req, res) => {
+
+    if (req.body.password !== req.body.confpass) {
+        res.render('signup.ejs', { loginmessage: "Passwords did not match."})
+    }
+
+    db.findOne({username: req.body.username}, (err, user) => {
+        if (err || user) {
+            res.render('signup.ejs', { 
+                loginmessage: "A user with that username already exists."
+            })
+        }
+
+        db.insert({ 
+            username: req.body.username,
+            password: req.body.password,
+            isadmin: false
+        },(err) => {
+            if (err) {
+                res.render('signup.ejs', { 
+                    loginmessage: "An error occured when creating the user."
+                })
+            }
+        })
+
+        console.log("Successfully created user." + 
+        "\nUsername: " + req.body.username +
+        "\nPassword: " + req.body.password)
+        res.redirect('/')
+        
+    })
+
+})
+
 app.get('/member', MemberAuth, (req, res) => {
     res.render('member.ejs')
 })
 
 app.get('/admin', AdminAuth, (req, res) => {
-    res.render('admin.ejs')
+    db.find({}, (err, users) => {
+        if (err) {res.end("An error occured")}
+        res.render('admin.ejs', { users: users})
+    })
 })
 
-app.listen(80, () => {})
+app.listen(80, () => {console.log("Now listening for incoming connections.")})
 
 function MemberAuth(req, res, next) {
+
     if (req.cookies.login_token) {
+
         jwt.verify(req.cookies.login_token, jwtsecret, (err) => {
+
             if (err) {
                 return res.sendStatus(403)
             } else {
                 next()
             }
+
         })
+
     } else {
         return res.sendStatus(401)
     }
 }
 
 function AdminAuth(req, res, next) {
+
     if (req.cookies.login_token) {
+
         jwt.verify(req.cookies.login_token, jwtsecret, (err, user) => {
+
             if (err || !user.isadmin === true) {
                 return res.sendStatus(403)
             } else {
                 next()
             }
+
         })
+
     } else {
         return res.sendStatus(401)
     }
